f24309a033f82896aa060393cb08322a60aa7b69
CyrielCheckout <cyrielghendrih-feillant@checkout.com>
2024-02-27 12:12:34 +0100

    add log data

--- server/controller/Cat_API/CAT.GetMerchantConf.js
+++ server/controller/Cat_API/CAT.GetMerchantConf.js
@@ -1,7 +1,7 @@
 const CATEntity = require('./CAT.EntityConf');
 const CATProcessingChannel = require('./CAT.ProcessingChannelConf');
 const waitfor = require('../IdempotencyKey');
-
+const logger = require('../../Utils/logger').logger;
 async function GetConf(body) {
   MerchantConfFinal = { "Entity": [] };
   //MerchantConfFinal.processorlist = []
@@ -9,6 +9,7 @@ async function GetConf(body) {
     GetAllEntity = await CATEntity.GetAllEntity(body.Bearer, body.ClientId, 25);
     console.log("NB entity :", GetAllEntity.body.total_count);
     if (GetAllEntity.body.total_count > 25) {
+      logger.log( 'info','GetMerchantConf for '+body.ClientId+', Number of entity > 25' ,"CAT_API");
       NBpage = Math.ceil(GetAllEntity.body.total_count / 25);
       console.log("Nb of pages :", NBpage);
       skip = 0;
@@ -17,13 +18,14 @@ async function GetConf(body) {
         GetAllEntity = await CATEntity.GetAllEntity(body.Bearer, body.ClientId, skip);
         console.log("getallentity lenth:", GetAllEntity.body._embedded.entities.length)
         for (let i = 0; i < GetAllEntity.body._embedded.entities.length; i++) {
-          console.log({ "EntityName": GetAllEntity.body._embedded.entities[i].name, "EntityID": GetAllEntity.body._embedded.entities[i].id, "status": GetAllEntity.body._embedded.entities[i].status })
+          logger.log( 'info','GetMerchantConf for '+body.ClientId+', Result = '+{ "EntityName": GetAllEntity.body._embedded.entities[i].name, "EntityID": GetAllEntity.body._embedded.entities[i].id, "status": GetAllEntity.body._embedded.entities[i].status } ,"CAT_API");
           MerchantConfFinal.Entity.push({ "EntityName": GetAllEntity.body._embedded.entities[i].name, "EntityID": GetAllEntity.body._embedded.entities[i].id, "status": GetAllEntity.body._embedded.entities[i].status });
         }
         skip = skip + 25;
       }
     }
     else {
+      logger.log( 'info','GetMerchantConf for '+body.ClientId+', Number of entity < 25' ,"CAT_API");
       GetAllEntity = await CATEntity.GetAllEntity(body.Bearer, body.ClientId, 0);
       for (let i = 0; i < GetAllEntity.body._embedded.entities.length; i++) {
         console.log({ "EntityName": GetAllEntity.body._embedded.entities[i].name, "EntityID": GetAllEntity.body._embedded.entities[i].id, "status": GetAllEntity.body._embedded.entities[i].status })
@@ -32,7 +34,7 @@ async function GetConf(body) {
       }
     };
     if (MerchantConfFinal.Entity.length === GetAllEntity.body.total_count) {
-      console.log("Count match go next step")
+      logger.log( 'info','GetMerchantConf for '+body.ClientId+', Count match go next step' ,"CAT_API");
       for (let entNumb = 0; entNumb < MerchantConfFinal.Entity.length; entNumb++) {
         try {
           MerchantConfFinal.Entity[entNumb].Processing_channel = []
@@ -59,7 +61,7 @@ async function GetConf(body) {
               MerchantConfFinal.Entity[entNumb].Processing_channel[PCNumb].PaymentMethod.push(err);
             }
           }
-          //Get Currency Account List
+          /*//Get Currency Account List
           try {
             CurrencyAccountList = await CATEntity.GetCurrencyAccountList(body.Bearer, MerchantConfFinal.Entity[entNumb].EntityID);
             for (let NBCA = 0; NBCA < CurrencyAccountList.data._embedded.currency_accounts.length; NBCA++) {
@@ -69,18 +71,18 @@ async function GetConf(body) {
           catch (err) {
             console.log(err)
             MerchantConfFinal.Entity[entNumb].Currency_Account = "Error while get the list of Currency Account"
-          }
+          }*/
         }
         catch (err) {
-          //console.log(err)
+          console.log(err)
           finalresult = { "status": 500, "Message": err }
         }
       };
     }
     else {
-      console.log("Count mismatch ERROR")
-      console.log("Result count =", MerchantConfFinal.Entity.length)
-      console.log("Initial count =", GetAllEntity.body.total_count)
+      logger.log( 'info','GetMerchantConf for '+body.ClientId+', Count mismatch ERROR' ,"CAT_API");
+      logger.log( 'info','GetMerchantConf for '+body.ClientId+', Result count ='+ MerchantConfFinal.Entity.length ,"CAT_API");
+      logger.log( 'info','GetMerchantConf for '+body.ClientId+', Initial count ='+ GetAllEntity.body.total_count ,"CAT_API");
     }
     MerchantConfFinal['status'] = 200;
     MerchantConfFinal['result'] = "merchant found";
